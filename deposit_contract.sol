a18e7a0b12407bdbdf435405caf514acef5ee7aa
Alex Beregszaszi <alex@rtfs.hu>
2020-05-12 01:04:52 +0100

    Document the interface using @notice, @param and @return (Doxygen/Natspec commands)

--- deposit_contract.sol
+++ deposit_contract.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.6.0;
 
 // This interface is designed to be compatible with the Vyper version.
 interface IDepositContract {
+    /// @notice A processed deposit event.
     event DepositEvent(
         bytes pubkey,
         bytes withdrawal_credentials,
@@ -12,6 +13,12 @@ interface IDepositContract {
         bytes index
     );
 
+    /// @notice Submit a Phase 0 DepositData object.
+    /// @param pubkey A BLS12-381 public key.
+    /// @param withdrawal_credentials Commitment to a public key for withdrawals.
+    /// @param signature A BLS12-381 signature.
+    /// @param deposit_data_root The SHA-256 hash of the SSZ-encoded DepositData object.
+    /// Used as a protection against malformed input.
     function deposit(
         bytes calldata pubkey,
         bytes calldata withdrawal_credentials,
@@ -19,8 +26,12 @@ interface IDepositContract {
         bytes32 deposit_data_root
     ) external payable;
 
+    /// @notice Query the current deposit root hash.
+    /// @return root The deposit root hash.
     function get_deposit_root() external view returns (bytes32);
 
+    /// @notice Query the current deposit count.
+    /// @return count The deposit count encoded as a little endian 64-bit number.
     function get_deposit_count() external view returns (bytes memory);
 }
 
